0432f199ab71b82bbe5b842b90861799211f7c99
Mark Ertmer <mark.ertmer@gmail.com>
2022-04-10 16:44:30 -0600

    Update: users controller with helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
 
+  before_action :require_user, only: :show
+
   def new
-    # @user = User.new
   end
 
   def create
-    # user = User.new(name: params[:name], email: params[:email])
     user = User.new(user_params)
 
     if user.save
-      # redirect_to "/users/#{user.id}"
       session[:user_id] = user.id
       redirect_to "/dashboard"
     else
@@ -19,15 +18,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(session[:user_id])
+    @user = current_user
   end
 
   def discover
-    @user = User.find(session[:user_id])
+    @user = current_user
   end
 
   def movies
-    @user = User.find(session[:user_id])
+    @user = current_user
 
     if params[:q] == 'top rated'
       @movies = MovieFacade.top_movies
@@ -37,26 +36,9 @@ class UsersController < ApplicationController
   end
 
   def movie_show
-    @user = User.find(session[:user_id])
+    @user = current_user
     @movie = MovieFacade.details(params[:movie_id])
   end
-
-  # def login_form
-  # end
-  #
-  # def login_user
-  #   user = User.find_by(email: params[:email])
-  #
-  #   if user == nil
-  #     redirect_to "/login"
-  #     flash[:alert] = "ERROR: Email not found"
-  #   elsif user.authenticate(params[:password])
-  #     redirect_to "/users/#{user.id}"
-  #   else
-  #     redirect_to "/login"
-  #     flash[:alert] = "ERROR: Incorrect password"
-  #   end
-  # end
 
   private
 

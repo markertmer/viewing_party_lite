7b422bfa7bfbf2adec5b7cc0a3de43f3317d4f0d
Mark Ertmer <mark.ertmer@gmail.com>
2022-04-10 16:44:07 -0600

    Add: user methods to AppCont.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
   private
 
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    if !current_user
+      flash[:alert] = 'You must be logged-in to do that!'
+      redirect_to '/'
+    end
+  end
+
   def error_message(errors)
     errors.full_messages.join(', ')
   end
-  
+
 end

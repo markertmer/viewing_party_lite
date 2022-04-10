ca94a03100da30c0b7dd3a435f1a961846f4fc69
Mark Ertmer <mark.ertmer@gmail.com>
2022-04-10 15:47:10 -0600

    Update: routes to not have user id

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,22 @@ Rails.application.routes.draw do
   # resources :user, only: [:show]
   get "/register", to: "users#new"
   post "/users", to: "users#create"
-  get "/users/:id", to: "users#show"
-  get "/users/:id/discover", to: "users#discover"
-  get "/users/:id/movies", to: "users#movies"
-  post "/users/:id/movies", to: "users#movies"
-  get "/users/:id/movies/:movie_id", to: "users#movie_show"
+  # get "/users/:id", to: "users#show"
+  get "/dashboard", to: "users#show"
+  # get "/users/:id/discover", to: "users#discover"
+  get "/discover", to: "users#discover"
+  # get "/users/:id/movies", to: "users#movies"
+  get "/movies", to: "users#movies"
+  # post "/users/:id/movies", to: "users#movies"
+  post "/movies", to: "users#movies"
+  # get "/users/:id/movies/:movie_id", to: "users#movie_show"
+  get "/movies/:movie_id", to: "users#movie_show"
 
 
-  get "login", to: "users#login_form"
-  post "login", to: "users#login_user"
+  # get "login", to: "users#login_form"
+  # post "login", to: "users#login_user"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 
 end
